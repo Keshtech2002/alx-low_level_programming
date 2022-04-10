c303e72e65df2494d573bae2bc8007c5c61b8bfb
Keshtech2002 <keshtech2002@gmail.com>
2022-04-10 16:16:39 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
