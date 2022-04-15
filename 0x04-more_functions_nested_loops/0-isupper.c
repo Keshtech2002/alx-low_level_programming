1a55dad9a4807cafa68c47ada79022247304a8a5
Keshtech2002 <keshtech2002@gmail.com>
2022-04-14 17:25:17 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isupper - function to check if c is uppercase
+ *@c - character to be tracked
+ *
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 $$ c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
