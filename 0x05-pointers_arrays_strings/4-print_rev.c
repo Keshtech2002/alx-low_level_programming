67a264026da8d4d94693a09bf3dd00c2d8223584
Keshtech2002 <keshtech2002@gmail.com>
2022-04-25 22:25:03 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print strings in reverse and new line
+ *@s: string to print
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string followed by a new line
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = 0, n = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		n = length / 2;
+	else
+		n = (length + 1) / 2;
+
+	while (n < length)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
