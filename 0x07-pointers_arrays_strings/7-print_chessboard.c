f166b1ad64d4533c6ab1fd7dd3cc6d9024a55015
Keshtech2002 <keshtech2002@gmail.com>
2022-04-27 09:58:37 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - function to print a chess board
+ * @a: array
+ *
+ * Return: Void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	
+}
