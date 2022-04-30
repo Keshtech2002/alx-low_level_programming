a64466fc5aa60d08a4afe9931d8e09bdc412531b
Keshtech2002 <keshtech2002@gmail.com>
2022-04-30 14:30:04 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,32 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, flag = 0;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; ; i++)
 	{
 		if (s1[i] == s2[i])
 		{
-			continue;
+			if (s1[i] == '\0')
+				return (0);
 		}
-		else if (s1[i] > s2[i])
-		{
-			flag = 1;
-			break;
-		}
-		else if (s1[i] < s2[i])
-		{
-			flag = -1;
-			break;
-		}
+		else
+			return (s1[i] - s2[i]);
 	}
-
-	if (flag == 1)
-	{
-		return (1);
-	}
-	else if (flag == -1)
-	{
-		return (-1);
-	}
-
-	return (0);
 }
