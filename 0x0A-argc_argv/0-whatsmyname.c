0a564c1685877f27adb1c7f7989b6912eafa3004
Keshtech2002 <keshtech2002@gmail.com>
2022-05-04 10:10:22 -0700

    t0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main code function
+ * @argc: arguments count
+ * @argv: argument vectors
+ * Return: void
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
