355bcd7bb0e031e406efd0011b5bb8839dc9a735
Keshtech2002 <keshtech2002@gmail.com>
2022-05-17 04:25:31 -0700

    t1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(li, int));
+		printf("%d", va_arg(li, int));
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
