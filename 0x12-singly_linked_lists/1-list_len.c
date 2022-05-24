403dc96eee17f353993574393944ff3ed27b4b16
Keshtech2002 <keshtech2002@gmail.com>
2022-05-24 16:32:31 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of nodes
+ * @h: pointer to linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+	
+	i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
