949e133921a0808388f3b1d6223edb5584e30f92
Keshtech2002 <keshtech2002@gmail.com>
2022-05-24 21:46:46 -0700

    task100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "list.h"
+
+/**
+ * _before_main - prints stuff before main
+ */
+void _before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
